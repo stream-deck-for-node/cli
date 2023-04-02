70d9317d5e576c2787a6a12637b7891bd963dbbb
Francesco Saverio Cannizzaro <francescosaverio.cannizzaro@gmail.com>
2023-04-02 14:15:16 +0200

    - update package flow

--- src/commands/package.ts
+++ src/commands/package.ts
@@ -10,10 +10,11 @@ import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { exec } from 'child_process';
 import logSymbols from 'log-symbols';
-import { stat } from 'fs/promises';
+import { mkdir, stat } from 'fs/promises';
 import got from 'got';
 import unzipper from 'unzipper';
 import { createWriteStream } from 'fs';
+import ora from 'ora';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -31,16 +32,24 @@ export default class PackageCommand implements CliCommand {
     const exist = await stat(dt).catch(() => false);
 
     if (!exist) {
-      got(ElgatoDistributionToolDownload, { isStream: true })
-        .pipe(unzipper.Parse())
-        .on('entry', (entry) => {
-          const fileName = entry.path;
-          if (fileName === DistributionTool) {
-            entry.pipe(createWriteStream(dt));
-          } else {
-            entry.autodrain();
-          }
-        });
+      const spinner = ora('Downloading Elgato Distribution Tool...').start();
+      return new Promise((resolve, reject) => {
+        got(ElgatoDistributionToolDownload, { isStream: true })
+          .pipe(unzipper.Parse())
+          .on('error', reject)
+          .on('entry', async (entry) => {
+            const fileName = entry.path;
+            if (fileName === DistributionTool) {
+              await mkdir(basePath, { recursive: true }).catch();
+              entry.pipe(createWriteStream(dt)).on('finish', () => {
+                spinner.stop();
+                resolve(dt);
+              });
+            } else {
+              entry.autodrain();
+            }
+          });
+      });
     }
 
     return dt;
@@ -52,6 +61,8 @@ export default class PackageCommand implements CliCommand {
     const dt = await this.downloadDistributionTool();
 
     if (manifest) {
+      const spinner = ora('Packaging plugin...').start();
+
       try {
         const uuid = getUUID(manifest);
         const pluginDirectory = join(PluginPath, uuid + '.sdPlugin');
@@ -62,16 +73,19 @@ export default class PackageCommand implements CliCommand {
 
         stdout?.on('data', (data) => {
           if (data.includes('successfully exported')) {
+            spinner.stop();
             console.log(logSymbols.success, 'Plugin package exported');
           }
         });
 
         stdout?.on('data', (data) => {
           if (data.includes('Error:')) {
+            spinner.stop();
             console.log(logSymbols.error, `${data.split('Error:')[1]}\n`);
           }
         });
       } catch (e) {
+        spinner.stop();
         console.log(logSymbols.error, "Cannot export the plugin's package");
       }
     }
